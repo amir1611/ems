4bf3700fcc5d8d3f385e2ccfb6f88591cc3e7bb8
chuaK <chuakianpheng@gmail.com>
2023-06-03 15:15:56 +0800

    route

--- routes/web.php
+++ routes/web.php
@@ -23,14 +23,32 @@ Auth::routes();
 
 Auth::routes(['verify' => true]);
 
-Route::group(['middleware' => ['auth', 'verified', 'user-role:admin']], function () {
-	Route::get('/admin', [HomeController::class, 'indexAdmin'])->name('admin.home');
-});;
+Route::prefix('staff')->name('staff.')->group(function () {
+	Route::group(['middleware' => ['auth', 'verified', 'user-role:staff']], function () {
+		Route::get('/', [HomeController::class, 'indexStaff'])->name('home');
+		Route::get('/consultation/manage', [ConsultationController::class, 'manage'])->name('consultation.manage');
+		Route::get('/consultant/manage', [ConsultantController::class, 'manage'])->name('consultant.manage');
+		Route::get('/consultant/create', [ConsultantController::class, 'create'])->name('consultant.create');
+		Route::put('/consultant/store', [ConsultantController::class, 'store'])->name('consultant.store');
+	});
 
-Route::group(['middleware' => ['auth', 'verified', 'user-role:staff']], function () {
-	Route::get('/staff', [HomeController::class, 'indexStaff'])->name('staff.home');
-});;
+});
 
-Route::group(['middleware' => ['auth', 'verified', 'user-role:user']], function () {
-	Route::get('/user', [HomeController::class, 'indexUser'])->name('user.home');
-});;
+
+Route::prefix('user')->name('user.')->group(function () {
+	
+	Route::group(['middleware' => ['auth', 'verified', 'user-role:user']], function () {
+		Route::get('/user', [HomeController::class, 'indexUser'])->name('user.home');
+	});
+	
+});
+
+Route::prefix('admin')->name('admin.')->group(function () {
+	
+	Route::group(['middleware' => ['auth', 'verified', 'user-role:admin']], function () {
+		Route::get('/admin', [HomeController::class, 'indexAdmin'])->name('admin.home');
+	});
+	
+});
+
+
