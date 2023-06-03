aed76142d760f0c0443523d1d80fa5a716e24815
Amir <amirjr1611@gmail.com>
2023-06-03 13:33:22 +0800

    reroute dashboard

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -21,10 +21,26 @@ class UserSeeder extends Seeder
 				'staff_id' => 'stf321',
 				'email' => 'lauhacker98@hotmail.com',
 				'contact' => '0987654321',
-                'email_verified_at' => '2023-06-01 09:16:17',
+                'email_verified_at' => '2023-06-03 09:16:17',
 				'role' => 2,
 				'password' => bcrypt('1234'),
 			],
+
+            [
+				'ic' => '111122223333',
+				'name' => 'amir',
+				'gender' => 'male',
+				'staff_id' => 'stf2020',
+				'email' => 'sukujah@gmail.com',
+				'contact' => '0387451298',
+                'email_verified_at' => '2023-06-03 09:16:17',
+				'role' => 1,
+				'password' => bcrypt('1234'),
+			],
+
+
+
+
 		];
 
 		foreach ($users as $key => $user) {
