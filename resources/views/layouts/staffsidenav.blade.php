04e4704e6ce59e7b8baf70791ed6fe040626aa84
Amir <amirjr1611@gmail.com>
2023-06-03 14:25:01 +0800

    head name

--- resources/views/layouts/staffsidenav.blade.php
+++ resources/views/layouts/staffsidenav.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Responsive Admin Dashboard | Korsat X Parmaga</title>
+    <title>Staff</title>
     <!-- ======= Styles ====== -->
     <link rel="stylesheet" href="{{ asset('css/userdashboard.css') }}">
 </head>
