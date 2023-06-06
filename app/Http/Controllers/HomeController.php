0a5d07bdca61584d3a4060cfc645e8f3ba349907
Amir <amirjr1611@gmail.com>
2023-06-07 00:54:35 +0800

    change password user

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,37 +2,65 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class HomeController extends Controller
 {
-	/**
-	 * Create a new controller instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		$this->middleware('auth');
-	}
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
-	/**
-	 * Show the application dashboard.
-	 *
-	 * @return \Illuminate\Contracts\Support\Renderable
-	 */
-	public function indexUser()
-	{
-		return view('manageProfile.userprofile');
-	}
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function indexUser()
+    {
+        return view('manageProfile.userprofile');
+    }
 
-	public function indexStaff()
-	{
-		return view('manageProfile.staff');
-	}
+    public function indexStaff()
+    {
+        return view('manageProfile.staff');
+    }
 
-	public function indexAdmin()
-	{
-		return view('manageProfile.admin');
-	}
+    public function indexAdmin()
+    {
+        return view('manageProfile.admin');
+    }
+
+
+    public function updatePassword(Request $request)
+    {
+        # Validation
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required|confirmed',
+        ]);
+
+        // dd($request);
+
+
+        #Match The Old Password
+        if (!Hash::check($request->old_password, auth()->user()->password)) {
+            return back()->with("error", "Old Password Doesn't match!");
+        }
+
+
+        #Update the new Password
+        User::whereId(auth()->user()->id)->update([
+            'password' => Hash::make($request->new_password)
+        ]);
+
+        return back()->with("status", "Password changed successfully!");
+    }
 }
--- resources/views/manageProfile/userprofile.blade.php
+++ resources/views/manageProfile/userprofile.blade.php
@@ -2,107 +2,117 @@
 
 
 @section('main-content')
+    <div class="container2" style="background-color: white;border-radius: 30px;">
+        <form action="{{ route('user.update', [auth()->user()->id]) }}" method="post">
+            @method('PUT')
+            @csrf
+            <div class="row mt-4 profile-header">
+                <h4 class="font-weight-bold mx-auto mt-2 profile-title">User Profile</h4>
+            </div>
+            <div class="table-responsive">
+                <table class="table mt-3 profile-table">
+                    <tr>
+                        <th class="col-md-4">IC Number</th>
+                        <td>{{ Auth::guard('web')->user()->ic }}</td>
+                    </tr>
+                    <tr>
+                        <th class="profile-label">Name</th>
+                        <td>
+                            <input class="form-control profile-input" type="text" name="name" id="name"
+                                value="{{ strtoupper(Auth::guard('web')->user()->name) }}">
+                        </td>
+                    </tr>
+                    <tr>
+                        <th class="profile-label">Gender</th>
+                        <td>
+                            <select class="form-control profile-input" name="gender" id="gender">
+                                <option value="male"
+                                    {{ Auth::guard('web')->user()->gender === 'male' ? 'selected' : '' }}>Male</option>
+                                <option value="female"
+                                    {{ Auth::guard('web')->user()->gender === 'female' ? 'selected' : '' }}>Female</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th class="profile-label">Phone Number</th>
+                        <td>
+                            <input class="form-control profile-input" type="text" name="contact" id="contact"
+                                value="{{ Auth::guard('web')->user()->contact }}">
+                        </td>
+                    </tr>
+                    <tr>
+                        <th class="profile-label">Email</th>
+                        <td>
+                            <input class="form-control profile-input" type="email" name="email" id="email"
+                                value="{{ Auth::guard('web')->user()->email }}">
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <div class="text-center">
+                <input class="btn profile-btn" type="submit" onclick="return confirm('Confirm to update profile?')"
+                    value="Edit Profile">
+            </div>
+            <br>
+            <br>
+        </form>
+    </div>
 
 
+    <div class="container2" style="background-color: white;border-radius: 30px;margin-top: 59px;margin-bottom: 20px">
+        {{-- <form action="{{ route('user.update', [auth()->user()->id]) }}" method="post"> --}}
+        <form action="{{ route('user.update-password') }}" method="POST">
 
-<div class="container2" style="background-color: white;border-radius: 30px;">
-    <form action="{{ route('user.update', [auth()->user()->id]) }}" method="post">
-        @method('PUT')
-        @csrf
-        <div class="row mt-4 profile-header">
-            <h4 class="font-weight-bold mx-auto mt-2 profile-title">User Profile</h4>
-        </div>
-        <div class="table-responsive">
-            <table class="table mt-3 profile-table">
-                <tr>
-                    <th class="col-md-4">IC Number</th>
-                    <td>{{ Auth::guard('web')->user()->ic }}</td>
-                </tr>
-                <tr>
-                    <th class="profile-label">Name</th>
-                    <td>
-                        <input class="form-control profile-input" type="text" name="name" id="name"
-                            value="{{ strtoupper(Auth::guard('web')->user()->name) }}">
-                    </td>
-                </tr>
-                <tr>
-                    <th class="profile-label">Gender</th>
-                    <td>
-                        <select class="form-control profile-input" name="gender" id="gender">
-                            <option value="male" {{ Auth::guard('web')->user()->gender === 'male' ? 'selected' : '' }}>Male</option>
-                            <option value="female" {{ Auth::guard('web')->user()->gender === 'female' ? 'selected' : '' }}>Female</option>
-                        </select>
-                    </td>
-                </tr>
-                <tr>
-                    <th class="profile-label">Phone Number</th>
-                    <td>
-                        <input class="form-control profile-input" type="text" name="contact" id="contact"
-                            value="{{ Auth::guard('web')->user()->contact }}">
-                    </td>
-                </tr>
-                <tr>
-                    <th class="profile-label">Email</th>
-                    <td>
-                        <input class="form-control profile-input" type="email" name="email" id="email"
-                            value="{{ Auth::guard('web')->user()->email }}">
-                    </td>
-                </tr>
-            </table>
-        </div>
-        <div class="text-center">
-            <input class="btn profile-btn" type="submit" onclick="return confirm('Confirm to update profile?')"
-                value="Edit Profile">
-        </div>
-        <br>
-        <br>
-    </form>
-</div>
+            @csrf
+            <div class="row mt-4 profile-header">
+                <h4 class="font-weight-bold mx-auto mt-2 profile-title">Change Password</h4>
+            </div>
+            <div class="table-responsive">
+                <table class="table mt-3 profile-table">
+
+                    <tr>
+                        <th class="profile-label">Old Password</th>
+                        <td>
+                            <input name="old_password" type="password"
+                                class="form-control profile-input @error('old_password') is-invalid @enderror"
+                                id="oldPasswordInput" placeholder="Old Password">
+                        </td>
+
+                        @error('old_password')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </tr>
+
+                    <tr>
+                        <th class="profile-label">New Password</th>
+                        <td>
+                            <input name="new_password" type="password"
+                                class="form-control profile-input @error('new_password') is-invalid @enderror"
+                                id="newPasswordInput" placeholder="New Password">
+                        </td>
+
+                        @error('new_password')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </tr>
+
+                    <tr>
+                        <th class="profile-label">Confirm New Password</th>
+                        <td>
+                            <input name="new_password_confirmation" type="password" class="form-control profile-input"
+                                id="confirmNewPasswordInput" placeholder="Confirm New Password">
+                        </td>
+                    </tr>
 
 
-<div class="container2" style="background-color: white;border-radius: 30px;margin-top: 59px;margin-bottom: 20px">
-    {{-- <form action="{{ route('user.update', [auth()->user()->id]) }}" method="post"> --}}
-        @method('PUT')
-        @csrf
-        <div class="row mt-4 profile-header">
-            <h4 class="font-weight-bold mx-auto mt-2 profile-title">Change Password</h4>
-        </div>
-        <div class="table-responsive">
-            <table class="table mt-3 profile-table">
-                <tr>
-                    <th class="profile-label">Curent Password</th>
-                    <td>
-                        <input class="form-control profile-input" type="text" name="name" id="name"
-                            value="{{ strtoupper(Auth::guard('web')->user()->name) }}">
-                    </td>
-                </tr>
-                <tr>
-                    <th class="profile-label">New Password</th>
-                    <td>
-                        <input class="form-control profile-input" type="text" name="name" id="name"
-                            value="{{ strtoupper(Auth::guard('web')->user()->name) }}">
-                    </td>
-                </tr>
-                <tr>
-                    <th class="profile-label">Confirm New Password</th>
-                    <td>
-                        <input class="form-control profile-input" type="text" name="name" id="name"
-                            value="{{ strtoupper(Auth::guard('web')->user()->name) }}">
-                    </td>
-                </tr>
-
-            </table>
-        </div>
-        <div class="text-center">
-            <input class="btn profile-btn" type="submit" onclick="return confirm('Confirm to change password?')"
-                value="Change Password">
-        </div>
-        <br>
-        <br>
-    </form>
-</div>
-
-
-
-
+                </table>
+            </div>
+            <div class="text-center">
+                <input class="btn profile-btn" type="submit" onclick="return confirm('Confirm to change password?')"
+                    value="Change Password">
+            </div>
+            <br>
+            <br>
+        </form>
+    </div>
 @endsection
