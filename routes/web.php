f1d1450d226a4c0ca780799542d293b5885cfcb2
pianburp <fiansuf@gmail.com>
2023-06-14 18:44:03 +0800

    tadi ada conflict, aku re-merge

--- routes/web.php
+++ routes/web.php
@@ -53,7 +53,7 @@ Route::prefix('staff')->name('staff.')->group(function () {
         Route::put('/consultant/store', [ConsultantController::class, 'store'])->name('consultant.store');
         Route::get('/incentive/view', [IncentiveController::class, 'view'])->name('incentive.view');
         //Route::delete('/incentive/delete/{id}, methods={"DELETE", "GET"}', [IncentiveController::class, 'delete'])->name('incentive.delete');
-        //pian
+        //pianhensem
         Route::put('/manageCard/manage', [MarriageCardController::class, 'manage'])->name('manageCard.manage');
 
     });
